fee30a07f0393b1dadd399f525fe6b022dc1d362
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-10 18:43:35 +0200

    add middleware to valited token, add refresh token, advert controller

--- jobBoardBackend/src/controllers/advertissement.controller.js
+++ jobBoardBackend/src/controllers/advertissement.controller.js
@@ -1,11 +1,26 @@
-
+const database = require('../models/index');
 
 const getAllAdvertissements = async (req, res) => {
-
+    try {
+        const advertissement = await database.sequelize.models.advertissement.findAll();
+        if (advertissement) {
+            return res.status(200).json(advertissement);
+        }
+        return res.status(404).send('Advertissements does not exists');
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
 }
 
 const createAdvertissement = async (req, res) => {
-
+    try {
+        const advertissement = await database.sequelize.models.advertissement.create(req.body);
+        if (advertissement) {
+            return res.status(201).json(advertissement);
+        }
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
 }
 
 const updateAdvertissement = async (req, res) => {
@@ -13,7 +28,17 @@ const updateAdvertissement = async (req, res) => {
 }
 
 const deleteAdvertissement = async (req, res) => {
-
+    try {
+        const { id } = req.params;
+        const deleted = await database.sequelize.models.advertissement.destroy({
+            where: { advertissement_id: Number(id) }
+        });
+        if (deleted) {
+            return res.status(204).send("Advertissement deleted");
+        }
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
 }
 
 module.exports = {
