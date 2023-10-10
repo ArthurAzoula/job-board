d7a4cb208358d7449904cd6832ea2631d1bcb965
Olivier PAKOSZ <olivier.pakosz@epitech.eu>
2023-10-10 18:02:20 +0200

    Ajout des éléments Navbar

--- jobBoardFrontend/src/components/Navbar.component.jsx
+++ jobBoardFrontend/src/components/Navbar.component.jsx
@@ -2,35 +2,35 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav className="bg-cerulean p-4">
-      <div className="mx-auto container w-full">
-        <div className="flex items-center justify-between w-full">
-          <a href="/" className="text-dogwood text-2xl font-bold">
-            Mon Site
-          </a>
-          <ul className="flex space-x-4">
-            <li>
-              <a href="/" className="text-dogwood hover:text-gunmetal">
-                Accueil
-              </a>
-            </li>
-            <li>
-              <a href="/about" className="text-dogwood hover:text-gunmetal">
-                À propos
-              </a>
-            </li>
-            <li>
-              <a href="/services" className="text-dogwood hover:text-gunmetal">
-                Services
-              </a>
-            </li>
-            <li>
-              <a href="/contact" className="text-dogwood hover:text-gunmetal">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
+    <nav className="bg-gunmetal p-4">
+      <div className="mx-auto container w-full flex items-center justify-between">
+        <a href="/" className="text-dogwood text-2xl font-bold text-center">
+          {" "}
+          {/* Utilisation de text-center */}
+          JobBoard de l'Entrepote
+        </a>
+        <ul className="flex space-x-4">
+          <li className="ml-auto">
+            <a href="/" className="text-dogwood hover:text-gunmetal">
+              Accueil
+            </a>
+          </li>
+          <li className="ml-auto">
+            <a href="/about" className="text-dogwood hover:text-gunmetal">
+              À propos
+            </a>
+          </li>
+          <li className="ml-auto">
+            <a href="/services" className="text-dogwood hover:text-gunmetal">
+              Services
+            </a>
+          </li>
+          <li className="ml-auto">
+            <a href="/contact" className="text-dogwood hover:text-gunmetal">
+              Contact
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
