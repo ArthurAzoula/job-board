360b76ac60059b2a0e0aafc718c6e6d6ee018bdc
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-10 14:39:28 +0200

    Make models, migration, relation between tables, functionnal

---
+++ jobBoardBackend/src/models/advertissement.model.js
@@ -0,0 +1,46 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class advertissementModel extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      advertissementModel.belongsTo(models.company)
+    }
+  }
+  advertissementModel.init({
+    advertissement_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+    },
+    titre: {
+        allowNull: false,
+        type: DataTypes.STRING(50),
+    },
+    description: {
+      allowNull: false,
+      type: DataTypes.STRING(255),
+    },
+    publication_date: {
+      allowNull: false,
+      type: DataTypes.DATE,
+
+    },
+    expiration_date: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  }, {
+    sequelize,
+    modelName: 'advertissement',
+  });
+  return advertissementModel;  
+};
