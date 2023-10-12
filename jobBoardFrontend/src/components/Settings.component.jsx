440f180ac505f705d4ed720c238664e08685f8f3
Olivier PAKOSZ <olivier.pakosz@epitech.eu>
2023-10-12 14:49:20 +0200

    Receive the data of the user connected

--- jobBoardFrontend/src/components/Settings.component.jsx
+++ jobBoardFrontend/src/components/Settings.component.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import { accountService } from "../services/account.service";
 import axios from "axios";
-import { get } from "../../../jobBoardBackend/src/routes/people.route";
+
 
 const Settings = () => {
   const [logged, setLogged] = useState(accountService.isLogged());
-  const [user, setUser] = UseState([]);
+  const [user, setUser] = useState([]);
 
   const token = accountService.getToken() || null;
 
@@ -16,19 +16,25 @@ const Settings = () => {
   // Afficher les infos du user sinon afficher veuillez vous connecter
   useEffect(() => {
     axios({
-      method: get,
-      url: `localhost:3000/api/users/me/${token}`,
+      method: 'GET',
+      url: `http://localhost:3000/api/users/me/${token}`,
       responseType: "json",
     }).then((response) => {
       if (response.status === 200) {
+        console.log(response.data)
         setUser(response.data);
       }
     }).catch(err => console.log(err))
-  });
+  }, []);
 
   return (
-    <>
-    </>
+    <div>
+      <li>{user.email}</li>
+      <li>{user.prenom}</li>
+      <li>{user.nom}</li>
+      <li>{user.telephone}</li>
+      <li>{user.createdAt}</li>
+    </div>
   );
 };
 
