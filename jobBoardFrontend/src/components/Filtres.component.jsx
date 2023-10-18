b3789c2f43de4698e569880118ed539b914a192b
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-18 16:31:36 +0200

    Responsive on filters components

--- jobBoardFrontend/src/components/Filtres.component.jsx
+++ jobBoardFrontend/src/components/Filtres.component.jsx
@@ -40,40 +40,37 @@ const Filtres = () => {
     e.preventDefault();
 
     console.log(credentials);
-  
+
     try {
       const response = axios.get(`http://localhost:3000/api/advertissements/filters?keywords=${credentials.titre}&type_contrat=${credentials.type_contrat}`)
       console.log(response.data);
     } catch (error) {
       console.error(error);
     }
-    
-
-    
   };
 
   return (
-    <div className="flex flex-col md:flex-row pt-4 pb-4  w-2/3 justify-center items-center bg-grisclair rounded-lg">
-      <form className="flex space-x-4" onSubmit={handleSubmit}>
-        <div className="elative flex items-center ml-4 pr-8">
-          <KeyWordsIcon className="absolute left-2 bg-gray-400 text-gray-400" />
+    <div className="flex flex-col sm:flex-row pt-4 pb-4 w-2/3 justify-center items-center bg-grisclair rounded-lg">
+      <form className="flex flex-col 2xl:flex-row sm:space-x-4 w-full sm:items-center" onSubmit={handleSubmit}>
+        <div className="relative flex md:ml-4 justify-center items-center mb-4 w-full sm:w-2/3 2xl:w-full 2xl:mb-0">
+          <KeyWordsIcon className="absolute left-2 ml-2 bg-gray-400 text-gray-400" />
           <span className="ml-2"></span>
           <input
             type="text"
             name="titre"
             id="titre"
             placeholder="Rechercher des mots clés (Développeur, Full-Stack, Back-end)"
-            className="pl-2 pr-2 py-2 w-96 rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            className="pl-2 pr-2 py-2 w-full rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             value={credentials.titre}
             onChange={onChange}
           />
         </div>
 
-        <div className="relative flex items-center pr-4">
+        <div className="relative flex justify-center items-center w-full sm:w-2/3 2xl:w-full mb-4 2xl:mb-0">
           <ContractIcon className="absolute left-2 text-gray-400" />
           <span className="ml-2"></span>
           <select
-            className="pl-2 py-2 w-48 rounded-md bg-white border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            className="pl-2 py-2 w-full rounded-md bg-white border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             name="type_contrat"
             id="type_contrat"
             value={credentials.type_contrat}
@@ -87,7 +84,7 @@ const Filtres = () => {
           </select>
         </div>
 
-        <div className="relative flex items-center">
+        <div className="relative flex justify-center items-center w-full sm:w-2/3 mb-4 md:mb-0">
           <LocalisationIcon className="absolute left-2 text-gray-400" />
 
           <span className="ml-1"></span>
@@ -96,16 +93,14 @@ const Filtres = () => {
             placeholder="Ville"
             name="lieu"
             id="lieu"
-            className="pl-2 pr-2 py-2 w-48 rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            className="pl-2 pr-2 py-2 w-full rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             value={city}
             onChange={handleCityChange}
           />
-        </div>
-        <div className="relative items-center">
           {suggestions.length > 0 && city.length > 0 && (
             <ul
-              className="absolute z-10 w-72 py-1 bg-white rounded-md shadow-lg border border-gray-300"
-              style={{ top: "40px", left: "-250px" }}
+              className="absolute z-10 w-full py-1 bg-white rounded-md shadow-lg border border-gray-300"
+              style={{ top: "40px", left: "0" }}
             >
               {suggestions.map((suggestion) => (
                 <li
@@ -119,14 +114,14 @@ const Filtres = () => {
             </ul>
           )}
         </div>
-        <div className="relative flex items-center">
-          <button className="bg-bleugris p-2 mr-4 rounded-full hover:scale-110 duration-200 text-white font-bold">
+        <div className="relative flex justify-center items-center">
+          <button className="bg-bleugris p-2 mr-4 mt-4 2xl:mt-0 rounded-full hover:scale-110 duration-200 text-white font-bold">
             <LoopIcon className="inline-block" />
           </button>
         </div>
       </form>
     </div>
   );
-};
+}
 
-export default Filtres;
+  export default Filtres;
